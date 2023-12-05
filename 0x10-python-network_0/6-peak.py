61e27769e5bc624a21b63a37c1d6533f8c041082
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-12-05 22:32:19 +0100

    ambiguous variable name resolved

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,21 +11,20 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    l = list_of_integers
     beg = 0
-    end = len(l)-1
+    end = len(list_of_integers)-1
 
-    if l[beg] > l[beg+1]:
-        return l[beg]
-    if l[end] > l[end-1]:
-        return l[end]
+    if list_of_integers[beg] > list_of_integers[beg+1]:
+        return list_of_integers[beg]
+    if list_of_integers[end] > list_of_integers[end-1]:
+        return list_of_integers[end]
 
     mid = (beg+end)//2
-    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
-        return l[mid]
-    if l[mid] < l[mid-1]:
-        return find_peak(l[beg:mid+1])
-    elif l[mid] < l[mid+1]:
-        return find_peak(l[mid:end+1])
+    if list_of_integers[mid-1] < list_of_integers[mid] and list_of_integers[mid+1] < list_of_integers[mid]:
+        return list_of_integers[mid]
+    if list_of_integers[mid] < list_of_integers[mid-1]:
+        return find_peak(list_of_integers[beg:mid+1])
+    elif list_of_integers[mid] < list_of_integers[mid+1]:
+        return find_peak(list_of_integers[mid:end+1])
     else:
-        return l[beg]
+        return list_of_integers[beg]
