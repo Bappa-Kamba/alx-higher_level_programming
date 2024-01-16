08a56d2dfbf91664e5b94bdc546f7cd1a222c4c3
BappaKamba <bappahmuhammad18@gmail.com>
2024-01-15 22:27:17 -0800

    late commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body).results;
+    const wedgeAtilleMovies = filmsDaa.filter(movie=>
+nst       movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+
+    console.log(`${wedgeAntillesMovies.length}`);
+  }
+});
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    try {
+      const todos = JSON.parse(body);
+      const completedTasksByUser = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          const userId = todo.userId;
+
+          if (completedTasksByUser[userId]) {
+            completedTasksByUser[userId]++;
+          } else {
+            completedTasksByUser[userId] = 1;
+          }
+        }
+      });
+
+      console.log(completedTasksByUser);
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
+
